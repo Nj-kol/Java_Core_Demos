2517c1e5ed0a44694c47a345b55d76302765925a
Nj-Kol <nilanjan.sarkar100@gmail.com>
2022-02-09 11:24:04 +0530

    latest

--- java-streams-demo/src/test/java/com/njkol/streams/TestCollectors.java
+++ java-streams-demo/src/test/java/com/njkol/streams/TestCollectors.java
@@ -4,15 +4,18 @@ import java.util.Comparator;
 import java.util.DoubleSummaryStatistics;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeSet;
 import java.util.concurrent.ForkJoinPool;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Stream;
 import static java.util.stream.Collectors.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
@@ -45,7 +48,7 @@ public class TestCollectors {
 	}
 
 	// Examples - https://stackabuse.com/guide-to-java-8-collectors-tomap/
-	//@Test
+	// @Test
 	public void testMapCollectors() {
 		var students = Arrays.asList(new Student("John", "Smith", "Miami", 7.38, 19),
 				new Student("Mike", "Miles", "New York", 8.4, 21),
@@ -67,24 +70,60 @@ public class TestCollectors {
 				.collect(toMap(Student::getName, Student::getAvgGrade, (a, b) -> (a + b) / 2, LinkedHashMap::new));
 	}
 
-	//@Test
+    @Test
+	public void testMapping() {
+		
+		var setStr = Stream.of("a", "a", "b")
+				.collect(mapping(String::toUpperCase, toSet()));
+		System.out.println(setStr); // [A, B]
+
+		var setStr1 = Stream.of("a", "a", "b")
+				.collect(flatMapping(s -> Stream.of(s.toUpperCase()), toSet()));
+		System.out.println(setStr1); // [A, B]
+	}
+    
+    @Test
+	public void testFiltering() {
+    	var strList2 = List.of("1", "2", "10", "100", "20", "999");
+    	var set = strList2.parallelStream()
+    			.collect(filtering(s -> s.length() < 2, toSet()));
+    	System.out.println(set); // [1, 2]
+	}
+    
+    
+    @Test
+	public void testReducing() {
+
+    	Map<Boolean, Optional<Integer>> reducing = Stream.of(1, 2, 3, 4, 5, 4, 3)
+    			.collect(partitioningBy(x -> x % 2 == 0, 
+    			 reducing(BinaryOperator.maxBy(Comparator.comparing(Integer::intValue)))));
+    	
+    	System.out.println(reducing); // {false=Optional[5], true=Optional[4]}
+	}
+    
+	// @Test
+	public void testCollectingAndThen() {
+		var strList2 = List.of("1", "2", "10", "100", "20", "999");
+
+		var unmodifiableList = strList2.parallelStream()
+				.collect(collectingAndThen(toList(), Collections::unmodifiableList));
+		System.out.println(unmodifiableList); // [1, 2, 10, 100, 20, 999]
+	}
+
+
+	// @Test
 	public void testJoining() {
 		var lengthOrder = Stream.of("Monkey", "Lion", "Giraffe", "Lemur").parallel();
 		// Good for concatenating strings in parallel
 		var o3 = lengthOrder.sorted(Comparator.comparing(String::length)).collect(joining());
 	}
 
-	//@Test
+	// @Test
 	public void testCounting() {
 		var lengthOrder = Stream.of("Monkey", "Lion", "Giraffe", "Lemur").parallel();
 
 		Long result = lengthOrder.collect(counting());
 		System.out.println("Number of elements : " + result);
-	}
-
-	// @Test
-	public void testCollectingAndThen() {
-
 	}
 
 	@Test
@@ -93,65 +132,55 @@ public class TestCollectors {
 				new Student("Mike", "Miles", "New York", 8.4, 21),
 				new Student("Michael", "Peterson", "New York", 7.5, 20),
 				new Student("James", "Robertson", "Miami", 9.1, 20), new Student("Kyle", "Miller", "Miami", 9.83, 20));
-		
-		//Using Collectors.summarizingDouble()
-	    DoubleSummaryStatistics doubleSummaryStatistics = students
-	        .stream()
-	        .collect(summarizingDouble(Student::getAvgGrade));
-	    
-	    System.out.println("DoubleSummaryStatistics for average grades: " + doubleSummaryStatistics);
+
+		// Using Collectors.summarizingDouble()
+		DoubleSummaryStatistics doubleSummaryStatistics = students.stream()
+				.collect(summarizingDouble(Student::getAvgGrade));
+
+		System.out.println("DoubleSummaryStatistics for average grades: " + doubleSummaryStatistics);
 	}
 
-	
 	@Test
 	public void testSumming() {
 		var students = Arrays.asList(new Student("John", "Smith", "Miami", 7.38, 19),
 				new Student("Mike", "Miles", "New York", 8.4, 21),
 				new Student("Michael", "Peterson", "New York", 7.5, 20),
 				new Student("James", "Robertson", "Miami", 9.1, 20), new Student("Kyle", "Miller", "Miami", 9.83, 20));
-		
-		//Using Collectors.summingDouble()
-	    Double sum = students
-	        .stream()
-	        .collect(summingDouble(Student::getAvgGrade));
-	    
-	    System.out.println("Sum of gardes: " + sum);
+
+		// Using Collectors.summingDouble()
+		Double sum = students.stream().collect(summingDouble(Student::getAvgGrade));
+
+		System.out.println("Sum of gardes: " + sum);
 	}
-	
+
 	@Test
 	public void testAvg() {
 		var students = Arrays.asList(new Student("John", "Smith", "Miami", 7.38, 19),
 				new Student("Mike", "Miles", "New York", 8.4, 21),
 				new Student("Michael", "Peterson", "New York", 7.5, 20),
 				new Student("James", "Robertson", "Miami", 9.1, 20), new Student("Kyle", "Miller", "Miami", 9.83, 20));
-		
-		//Using Collectors.averagingDouble()
-	    Double avg = students
-	        .stream()
-	        .collect(averagingDouble(Student::getAvgGrade));
-	    
-	    System.out.println("Average age of students: " + avg);
+
+		// Using Collectors.averagingDouble()
+		Double avg = students.stream().collect(averagingDouble(Student::getAvgGrade));
+
+		System.out.println("Average age of students: " + avg);
 	}
-	
+
 	@Test
 	public void testMinMax() {
-	
+
 		var students = Arrays.asList(new Student("John", "Smith", "Miami", 7.38, 19),
 				new Student("Mike", "Miles", "New York", 8.4, 21),
 				new Student("Michael", "Peterson", "New York", 7.5, 20),
 				new Student("James", "Robertson", "Miami", 9.1, 20), new Student("Kyle", "Miller", "Miami", 9.83, 20));
-		
-		//Using Collectors.maxBy()
-	    Optional<Student> eldestStudent = students
-	        .stream()
-	        .collect(maxBy(Comparator.comparing(Student::getAge)));
-	    
-	  //Using Collectors.minBy()
-	    Optional<Student> youngestStudent = students
-		        .stream()
-		        .collect(minBy(Comparator.comparing(Student::getAge)));
-		    
-	    System.out.println("The eldest Student is : " + eldestStudent.get());
-	    System.out.println("The youngest Student is : " + youngestStudent.get());
+
+		// Using Collectors.maxBy()
+		Optional<Student> eldestStudent = students.stream().collect(maxBy(Comparator.comparing(Student::getAge)));
+
+		// Using Collectors.minBy()
+		Optional<Student> youngestStudent = students.stream().collect(minBy(Comparator.comparing(Student::getAge)));
+
+		System.out.println("The eldest Student is : " + eldestStudent.get());
+		System.out.println("The youngest Student is : " + youngestStudent.get());
 	}
 }
